a7d817abef1b73f7aa41b58bca5ac55e28f88728
cxz <15900959052@163.com>
2020-06-07 23:07:38 +0800

    lastEvent

--- src/monitor/lib/jsError.js
+++ src/monitor/lib/jsError.js
@@ -1,7 +1,10 @@
-
+import getLastEvents from '../utils/getLastEvents'
 
 export function injectJsError () {
   window.addEventListener('error', (event) => { // 监听错误对象
+    let lastEvent = getLastEvents()
+    console.log(333, lastEvent);
+
     console.log('event', event)
     const { message, filename, lineno, colno, error: { stack } } = event
     let log = {
@@ -12,13 +15,15 @@ export function injectJsError () {
       message,
       filename,
       position: `${lineno}:${colno}`,
-      stack: getLines(stack)
-
+      stack: getLines(stack),
+      slector: ''//最后操作的一个元素
     }
+    console.log(111, log);
+
   })
 
   function getLines (stack) {
-    return stack.split('\n').slice(1).map(item => item.replace(/^\s+at\s+/g, '').join('^'))
+    return stack.split('\n').slice(1).map(item => item.replace(/^\s+at\s+/g, '')).join('^')
   }
 
 }
